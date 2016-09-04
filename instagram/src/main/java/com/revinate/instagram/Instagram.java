87ae37afc7c56fac0c9be5549f9aada3d61c6fda
Meghana Kothaluri <kothaluri07@gmail.com>
2016-09-04 13:33:36 -0500

    My solution for Instagram Challenge

--- instagram/src/main/java/com/revinate/instagram/Instagram.java
+++ instagram/src/main/java/com/revinate/instagram/Instagram.java
@@ -1,7 +1,6 @@
 package com.revinate.instagram;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Your Instagram object will be instantiated and called as such:
@@ -15,30 +14,87 @@ import java.util.List;
 public class Instagram {
 
     /** Initialize your data structure here. */
-    public Instagram() {
 
+    //ArrayList of feeds with userId and mediaId
+    List<List<Integer>> feedInfo;
+
+    //HashMap with followerId as key and List of followedIds as value
+    HashMap<Integer, List<Integer>> followsInfo;
+
+    public Instagram()
+    {
+        this.feedInfo = new ArrayList<List<Integer>>();
+        this.followsInfo = new HashMap<Integer, List<Integer>>();
     }
 
     /** Add a new media. */
-    public void postMedia(int userId, int mediaId) {
+    public void postMedia(int userId, int mediaId)
+    {
+        List<Integer> mediaInfo = new ArrayList<Integer>();
 
+        mediaInfo.add(userId);
+        mediaInfo.add(mediaId);
+
+        feedInfo.add(mediaInfo);
     }
 
     /** Retrieve the 10 most recent media ids in the user's media feed.
      * Each media must be posted by the user herself or by someone the user follows
      * Media must be ordered from most recent to least recent. */
-    public List<Integer> getMediaFeed(int userId) {
-        return Collections.emptyList();
-    }
+    public List<Integer> getMediaFeed(int userId)
+    {
+        List<Integer> resultFeed = new ArrayList<Integer>();
+
+        for(List<Integer> li : feedInfo)
+        {
+            List<Integer> tmpFollows = new ArrayList<Integer>();
+
+            if(followsInfo.containsKey(userId))
+            {
+                tmpFollows = followsInfo.get(userId);
+            }
+
+            if( (li.get(0) == userId) || (tmpFollows.contains(li.get(0))) )
+            {
+                resultFeed.add(li.get(1));
+            }
+        }
+
+        Collections.reverse(resultFeed); //to get most recent to least recent
+
+        if(resultFeed.size() > 10)
+        {
+            return resultFeed.subList(0,10); //displaying recent 10 feeds
+        }
+
+        return resultFeed;
+     }
 
     /** A Follower follows a followed. Nothing happens if invalid */
-    public void follow(int followerId, int followedId) {
+    public void follow(int followerId, int followedId)
+    {
+        if(followsInfo.containsKey(followerId))
+        {
+            followsInfo.get(followerId).add(followedId); //update follwedIds for a followerId
+        }
+        else
+        {
+            List<Integer> follows = new ArrayList<Integer>();
+            follows.add(followedId);
 
+            followsInfo.put(followerId, follows); //initial entry in HashMap
+        }
     }
 
     /** A Follower unfollows a followed. Nothing happens if invalid */
-    public void unfollow(int followerId, int followedId) {
-
+    public void unfollow(int followerId, int followedId)
+    {
+        if(followsInfo.containsKey(followerId))
+        {
+            List<Integer> tmpFollows = followsInfo.get(followerId);
+            int ind = tmpFollows.indexOf(followedId);
+            tmpFollows.remove(ind); //unfollows a followedId
+        }
     }
 }
 
--- instagram/src/test/java/com/revinate/instagram/InstagramTest.java
+++ instagram/src/test/java/com/revinate/instagram/InstagramTest.java
@@ -22,21 +22,44 @@ public class InstagramTest {
         Thread.sleep(1);
         instagram.postMedia(1,1022);
 
+        // testing if it displays not more than 10 recent feeds
+        Thread.sleep(1);
+        instagram.postMedia(3,1111);
+        Thread.sleep(1);
+        instagram.postMedia(2, 2222);
+        instagram.postMedia(3,3333);
+        Thread.sleep(1);
+        instagram.postMedia(3,4444);
+        instagram.postMedia(3,5555);
+        Thread.sleep(1);
+        instagram.postMedia(3,6666);
+        instagram.postMedia(3,7777);
+        Thread.sleep(1);
+        instagram.postMedia(3,8888);
+
         instagram.follow(1, 2);
         instagram.follow(1, 3);
 
         List<Integer> feed = instagram.getMediaFeed(1);
-        assertThat(feed.get(0), is(1022));
-        assertThat(feed.get(1), is(3094));
-        assertThat(feed.get(2), is(1092));
-        assertThat(feed.get(3), is(1135));
-        assertThat(feed.get(4), is(1025));
+        assertThat(feed.size(), is(10));
+        assertThat(feed.get(0), is(8888));
+        assertThat(feed.get(1), is(7777));
+        assertThat(feed.get(2), is(6666));
+        assertThat(feed.get(3), is(5555));
+        assertThat(feed.get(4), is(4444));
+        assertThat(feed.get(5), is(3333));
+        assertThat(feed.get(6), is(2222));
+        assertThat(feed.get(7), is(1111));
+        assertThat(feed.get(8), is(1022));
+        assertThat(feed.get(9), is(3094));
 
         instagram.unfollow(1, 3);
 
         List<Integer> feed_2 = instagram.getMediaFeed(1);
-        assertThat(feed_2.get(0), is(1022));
-        assertThat(feed_2.get(1), is(1135));
-        assertThat(feed_2.get(2), is(1025));
+        assertThat(feed_2.get(0), is(2222));
+        assertThat(feed_2.get(1), is(1022));
+        assertThat(feed_2.get(2), is(1135));
+        assertThat(feed_2.get(3), is(1025));
+
     }
 }
